1c88df5698eb51dd1ddc01ac7afe0d7a763da4f8
Niccolò Fontana <inico1@me.com>
2020-05-10 11:43:32 +0200

    day1: completed

---
+++ Day1/Day1.swift
@@ -0,0 +1,40 @@
+// Variables
+var str = "Hello Swift!"
+str = "I can mutate!"
+
+// Integers
+var population = 5_000_178
+
+// Multiline-Strings
+var correctMultiline = """
+This goes
+over multiple
+lines
+"""
+
+var incorrectMultiline = """
+I'm an
+incorrect multiline"""
+
+var escapedMultine = """
+I'm on \
+a single line!
+"""
+
+// Doubles and Boolean
+var pi = 3.141
+var awesome = true
+
+// String Interpolation
+var score = 85
+var str = "Your score is \(score)"
+
+// Constants
+let str = "I cannot mutate!"
+str = "Hello" // Error!
+
+// Type Annotation
+let album: String = "California"
+let year: Int = 2016
+let height: Double = 1.78
+let isDayOne: Bool = true
