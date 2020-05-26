9f1bc8719fd7b9e8140c6bc72deb0f5be5a11db7
Niccolò Fontana <inico1@me.com>
2020-05-26 16:24:08 +0200

    day17: implement StormViewer Pt 2

---
+++ Day16-18/StormViewer/StormViewer/DetailViewController.swift
@@ -0,0 +1,60 @@
+//
+//  DetailViewController.swift
+//  StormViewer
+//
+//  Created by Niccolò Fontana on 26/05/2020.
+//  Copyright © 2020 Niccolò Fontana. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    // When the basic stuff of DetailViewController's initialization
+    // has been done (allocating enough memory to hold it
+    // all, for example), iOS goes ahead and loads the layout from the
+    // storyboard, then connects all the outlets from the storyboard to the
+    // code.
+    // When the DetailViewController is first made, the UIImageView doesn't
+    // exist because it hasn't been created yet – but we still need to have
+    // some space for it in memory. At this point, the property is nil, or
+    // just some empty memory. But when the view gets loaded and the outlet
+    // gets connected, the UIImageView will point to a real UIImageView, not
+    // to nil, so we can start using it.
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = selectedImage
+        
+        // this is the configuration of the navigation bar only for this screen
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Day16-18/StormViewer/StormViewer/ViewController.swift
+++ Day16-18/StormViewer/StormViewer/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
         // File System interaction
         let fileManager = FileManager.default
         
@@ -32,14 +35,28 @@ class ViewController: UITableViewController {
         }
     }
     
+    // This code will be triggered when iOS wnats to know how many rows are in the TableView at the specified section
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return picturesNames.count
     }
 
+    // Triggered when iOS wants the cell at the specified IndexPath, IndexPath is a data type that contains both a section and a row number
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
         cell.textLabel?.text = picturesNames[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
+        let detailVC = storyboard?.instantiateViewController(identifier: "Detail") as! DetailViewController
+        
+        // 2: success! Set its selectedImage property
+        detailVC.selectedImage = picturesNames[indexPath.row]
+        
+        // 3: now push it onto the navigation controller
+        navigationController?.pushViewController(detailVC, animated: true)
+        // each new view controller that pushed onto the navigation controller stack inherits the style of its predecessor.
     }
 
 }
