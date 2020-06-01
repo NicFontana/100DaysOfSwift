d822ef5007415788b2f4e9ad6d2495355148430e
Niccolò Fontana <inico1@me.com>
2020-06-01 11:18:04 +0200

    day20: implement GuessTheFlag Pt 2

--- Day19-21/GuessTheFlag/GuessTheFlag/ViewController.swift
+++ Day19-21/GuessTheFlag/GuessTheFlag/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var button3: UIButton!
     
     var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,10 +35,41 @@ class ViewController: UIViewController {
         askQuestion()
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            score += 1
+            title = "Correct"
+        } else {
+            score -= 1
+            title = "Wrong"
+        }
+        
+        let alertController = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        
+        let continueAction = UIAlertAction(title: "Continue", style: .default, handler: askQuestion)
+        // let middleAction = UIAlertAction(title: "Middle", style: .default)
+        // let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        // let destructiveAction = UIAlertAction(title: "Destructive", style: .destructive)
+        
+        alertController.addAction(continueAction)
+        // alertController.addAction(middleAction)
+        // alertController.addAction(cancelAction)
+        // alertController.addAction(destructiveAction)
+        
+        present(alertController, animated: true)
     }
 }
 
